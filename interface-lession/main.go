658a9c2451e3ab6795995d373ad8def29da8f80e
liuyanhao <liuyanhao@industai.com>
2021-12-18 22:19:54 +0800

    feat: add

---
+++ interface-lession/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type People interface {
+	Speak(string) string
+}
+
+type Student struct{}
+
+func (stu *Student) Speak(think string) (talk string) {
+	if think == "sb" {
+		talk = "你是个大帅比"
+	} else {
+		talk = "您好"
+	}
+	return
+}
+
+func main() {
+	// var peo People = &Student{}
+	// think := "bitch"
+	// fmt.Println(peo.Speak(think))
+	var x interface{} = "你好呀"
+	v, ok := x.(string)
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("failed")
+	}
+}
